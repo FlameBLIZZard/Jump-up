556a77fb046f822733a5be8a15a9a86f4d71767b
Aditya Bhoite <81624487+FlameBLIZZard@users.noreply.github.com>
2026-04-29 22:07:38 +0530

    Hook main game script; adjust player & camera
    Add a new main_game.gd (and UID) and attach it to the Main-Game scene to reload the scene when the player falls below y=100. Populate the main scene with many additional ground Solid instances and enable unique naming for the Player instance. Tweak Camera2D (offset, zoom, smoothed limits and smoothing speed). Make character movement configurable by exporting SPEED and JUMP_VELOCITY in character_body_2d.gd (jump velocity changed from -200 to -300) and add a JUMP_VELOCITY property to player.tscn so it can be adjusted per-instance.

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
-
+@export var SPEED = 150.0
+@export var JUMP_VELOCITY = -300.0
+@onready var player: Node2D = $".."
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,9 +12,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
